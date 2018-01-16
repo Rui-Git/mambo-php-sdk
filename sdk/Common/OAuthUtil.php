0ed1361836faddeab1153684320b3ed04fe9fc91
Rui AVELINO <rui.avelino@gmail.com>
2018-01-16 14:12:58 +0100

    fix(class): call of class with full namespace

--- sdk/Common/OAuthUtil.php
+++ sdk/Common/OAuthUtil.php
@@ -5,7 +5,7 @@ namespace Mambo\Common;
 class MamboOAuthUtil {
     public static function urlencode_rfc3986($input) {
         if (is_array($input)) {
-            return array_map(array('MamboOAuthUtil', 'urlencode_rfc3986'), $input);
+            return array_map(array('Mambo\Common\MamboOAuthUtil', 'urlencode_rfc3986'), $input);
         } else if (is_scalar($input)) {
             return str_replace(
                     '+',
